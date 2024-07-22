4252ceb9c356e0e9cbcb662f59bec3af995fe571
Keno Hassler <40292329+kenohassler@users.noreply.github.com>
2024-07-23 01:05:36 +0200

    Clean up examples (#7)
    * simplify examples
    
    * improve CI (clippy + rustfmt)

--- examples/cli.rs
+++ examples/cli.rs
@@ -1,9 +1,8 @@
-use clap::Parser;
 use embedded_hal::delay::DelayNs;
 use embedded_io_adapters::std::FromStd;
 use inquire::Select;
 use sds011::{Config, SDS011};
-use serialport;
+use std::env;
 use std::error::Error;
 use std::thread::sleep;
 use std::time::Duration;
@@ -16,30 +15,19 @@ impl DelayNs for Delay {
     }
 }
 
-/// Simple CLI to poll the SDS011 fine particle sensor
-#[derive(Parser, Debug)]
-#[command(version)]
-struct Args {
-    /// Which serial port to use. Will be queried interactively if not given.
-    port: Option<String>,
-    /// Poll the sensor every n minutes, 0 for one-shot.
-    #[arg(short = 'n', long, default_value_t = 0)]
-    interval: u32,
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
+    let mut args: Vec<String> = env::args().collect();
 
-    let ans = match args.port {
-        Some(p) => p,
-        None => {
-            let ports = serialport::available_ports().expect("No ports found!");
-            let ports: Vec<String> = ports.into_iter().map(|p| p.port_name).collect();
-            Select::new("Which serial port should be used?", ports).prompt()?
-        }
+    // use the first arg as serial port, query interactively if not given
+    let port = if args.len() == 2 {
+        args.pop().unwrap()
+    } else {
+        let ports = tokio_serial::available_ports()?;
+        let ports: Vec<String> = ports.into_iter().map(|p| p.port_name).collect();
+        Select::new("Which serial port should be used?", ports).prompt()?
     };
 
-    let builder = serialport::new(ans, 9600).timeout(Duration::from_secs(1));
+    let builder = serialport::new(port, 9600).timeout(Duration::from_secs(1));
     let serial = builder.open()?;
 
     let mut adapter = FromStd::new(serial);
@@ -53,16 +41,6 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let vals = sensor.measure(&mut Delay)?;
     println!("{vals}");
-
-    // continuously measure every n minutes (taking 30s measurement delay into account)
-    if args.interval != 0 {
-        loop {
-            Delay.delay_ms((args.interval * 60 - 30) * 1000);
-
-            let vals = sensor.measure(&mut Delay)?;
-            println!("{vals}");
-        }
-    }
 
     Ok(())
 }
--- examples/cli_async.rs
+++ examples/cli_async.rs
@@ -1,8 +1,8 @@
-use clap::Parser;
 use embedded_hal_async::delay::DelayNs;
 use embedded_io_adapters::tokio_1::FromTokio;
 use inquire::Select;
 use sds011::{Config, SDS011};
+use std::env;
 use std::error::Error;
 use std::time::Duration;
 use tokio::time::sleep;
@@ -16,31 +16,20 @@ impl DelayNs for Delay {
     }
 }
 
-/// Simple CLI to poll the SDS011 fine particle sensor (async version)
-#[derive(Parser, Debug)]
-#[command(version)]
-struct Args {
-    /// Which serial port to use. Will be queried interactively if not given.
-    port: Option<String>,
-    /// Poll the sensor every n minutes, 0 for one-shot.
-    #[arg(short = 'n', long, default_value_t = 0)]
-    interval: u32,
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
+    let mut args: Vec<String> = env::args().collect();
 
-    let ans = match args.port {
-        Some(p) => p,
-        None => {
-            let ports = tokio_serial::available_ports().expect("No ports found!");
-            let ports: Vec<String> = ports.into_iter().map(|p| p.port_name).collect();
-            Select::new("Which serial port should be used?", ports).prompt()?
-        }
+    // use the first arg as serial port, query interactively if not given
+    let port = if args.len() == 2 {
+        args.pop().unwrap()
+    } else {
+        let ports = tokio_serial::available_ports()?;
+        let ports: Vec<String> = ports.into_iter().map(|p| p.port_name).collect();
+        Select::new("Which serial port should be used?", ports).prompt()?
     };
 
-    let builder = tokio_serial::new(ans, 9600).timeout(Duration::from_secs(1));
+    let builder = tokio_serial::new(port, 9600).timeout(Duration::from_secs(1));
     let serial = SerialStream::open(&builder)?;
 
     let mut adapter = FromTokio::new(serial);
@@ -54,16 +43,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let vals = sensor.measure(&mut Delay).await?;
     println!("{vals}");
-
-    // continuously measure every n minutes (taking 30s measurement delay into account)
-    if args.interval != 0 {
-        loop {
-            Delay.delay_ms((args.interval * 60 - 30) * 1000).await;
-
-            let vals = sensor.measure(&mut Delay).await?;
-            println!("{vals}");
-        }
-    }
 
     Ok(())
 }
